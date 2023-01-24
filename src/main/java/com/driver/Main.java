0e7a13ee356f0b5be4f1443d19a6f8b9a0ac2a0d
mastanvali13111 <mastanvali13111@gmail.com>
2023-01-24 19:04:06 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,22 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(4,5)+" "+p.product(3,5,6)+" "+p.product(2.3,4.6));
+    }
+    public static class Product{
+        public  static int product(int x, int y) {
+            return x*y;
+        }
+        public  static int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public  static double product(double x, double y) {
+            return  x*y;
+        }
+    }
+
+
 }
